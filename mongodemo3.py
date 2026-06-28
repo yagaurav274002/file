dc44a5210c8d7caddfb82c7eec80680363a04da8
yagaurav274002 <gy500934@gmail.com>
2026-06-28 20:22:40 +0530

    Delete mongodemo3.py

--- mongodemo3.py
+++
@@ -1,7 +0,0 @@
-import pymongo
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mybigdata"]
-mycol=mydb["student"]
-mydict={"name":"vai", "address":"bhy"}
-x=mycol.insert_one(mydict)
-print("Data inserted !")
