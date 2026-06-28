8f31fe524cf1e33c0a6e4ad877ce8a04aba9bad1
yagaurav274002 <gy500934@gmail.com>
2026-06-28 20:23:09 +0530

    Delete mongodemo5.py

--- mongodemo5.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mybigdata"]
-mycol=mydb["student"]
-mylist=[{"name":"Ganesh", "address":"Mumbai"}, {"name":"Varun", "address":"Mumbai"},
-{"name":"Prasoon", "address":"Pune"}, {"name":"Satish", "address":"Pune"},]
-x=mycol.insert_many(mylist)
-print("Data inserted !")
